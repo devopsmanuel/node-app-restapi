59cf9a76b32191e43a1dbda3d01a5a81295a471a
Devops Manuel <devops.manuel@gmail.com>
2023-06-02 10:43:55 -0500

    add formar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import express from "express"
-import usuarioRoutes from "./routes/usuario.routes.js"
-import ticketRoutes from "./routes/ticket.routes.js"
+import express from 'express'
+import usuarioRoutes from './routes/usuario.routes.js'
+import ticketRoutes from './routes/ticket.routes.js'
 import swaggerJSDocV1 from './swagger.js'
 import cors from 'cors'
 
@@ -11,8 +11,8 @@ const PORT = 3000
 app.use(cors())
 app.use(express.json())
 
-app.use("/api/v1", usuarioRoutes)
-app.use("/api/v1", ticketRoutes)
+app.use('/api/v1', usuarioRoutes)
+app.use('/api/v1', ticketRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server on port: ${PORT}`)
--- src/routes/ticket.routes.js
+++ src/routes/ticket.routes.js
@@ -1,9 +1,9 @@
-import { Router } from "express"
-import { prisma } from "../db.js"
+import { Router } from 'express'
+import { prisma } from '../db.js'
 
 const router = Router()
 
-router.get("/ticket", async (req, res, next) => {
+router.get('/ticket', async (req, res, next) => {
 	try {
 		const ticket = await prisma.ticket.findMany();
 		res.json(ticket)
